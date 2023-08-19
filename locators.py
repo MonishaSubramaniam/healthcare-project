1d892f60fb421fe4689f4466508d5bfcc3a7a3b0
MonishaSubramaniam <141481075+MonishaSubramaniam@users.noreply.github.com>
2023-08-19 08:26:27 -0700

    Add files via upload

---
+++ locators.py
@@ -0,0 +1,8 @@
+class Locators:
+    make_apnt='btn-make-appointment'  #ID
+    username_locator = 'txt-username' # ID
+    password_locator = 'txt-password' # ID
+    submit_button = 'btn-login' # ID
+    date="//input[@placeholder='dd/mm/yyyy']"
+    com='txt_comment'
+    Book='btn-book-appointment'
